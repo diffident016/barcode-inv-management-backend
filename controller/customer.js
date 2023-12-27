e96384cdfc3bb702104b01fb9a54983fce328d46
diffident016 <marvin.tagolimotjr13@gmail.com>
2023-12-27 18:02:56 +0800

    feat: customer auth routes

--- controller/customer.js
+++ controller/customer.js
@@ -1,18 +1,41 @@
 import Customer from "../models/customers.js";
+import bcrypt from 'bcryptjs'
 
 const getCustomer = async (req, res) => {
-    const customer = await Customer.findOne({
-        email: req.params.email
-    });
-    res.json(customer);
+
+
+    try {
+        const customer = await Customer.findOne({
+            email: req.body.email
+        });
+
+        if (!customer) {
+            return res.status(201).json('Account not found.');
+        }
+
+        const isPasswordValid = bcrypt.compareSync(req.body.password, customer.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json('Invalid username or password.');
+        }
+
+        res.json(customer);
+
+    } catch (e) {
+        console.log(e)
+        return res.status(201).json('Account not found.');
+    }
 };
 
 const addCustomer = async (req, res) => {
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, '@diffident016')
+
     let addCustomer = new Customer({
         name: req.body.name,
         email: req.body.email,
         userType: req.body.userType,
-        password: req.body.password,
+        password: hashedPassword,
         imageUrl: req.body.imageUrl,
     });
 
@@ -22,7 +45,6 @@ const addCustomer = async (req, res) => {
             res.status(200).send(result);
         })
         .catch((err) => console.log("Signup: ", err));
-    console.log("request: ", req.body);
 };
 
 export { addCustomer, getCustomer }
--- router/customer.js
+++ router/customer.js
@@ -2,7 +2,7 @@ import express from "express";
 import { addCustomer, getCustomer } from "../controller/customer.js";
 const app = express();
 
-app.get("/login", getCustomer)
+app.post("/login", getCustomer)
 app.post("/register", addCustomer)
 
 export default app;
